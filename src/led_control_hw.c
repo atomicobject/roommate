eb8fc3227ac03deedc486b92639904b492bee675
Jordan Schaenzle <jschaenzle@gmail.com>
2018-08-20 21:04:08 -0400

    Working on talking to LEDs

--- src/led_control_hw.c
+++ src/led_control_hw.c
@@ -8,7 +8,7 @@
 
 #define NUM_LEDS 8
 #define BITS_PER_LED_CMD 24 
-#define LED_BUFFER_ITEMS (NUM_LEDS * BITS_PER_LED_CMD)
+#define LED_BUFFER_ITEMS ((NUM_LEDS * BITS_PER_LED_CMD) + 2) // Two extra for low period at start and end of sequence
 
 
 // typedef struct {
@@ -32,7 +32,7 @@
 
 rmt_item32_t led_data_buffer[LED_BUFFER_ITEMS];
 
-void init_led_data_buffer(void);
+void init_led_data_buffer(int a);
 void rmt_send_finished_isr(void *p_data);
 
 void led_control_hw_init()
@@ -48,7 +48,7 @@ void led_control_hw_init()
   // if the RMT_TX_GPIO is connected to a speaker
   config.tx_config.carrier_en = false;
   config.tx_config.idle_output_en = true;
-  config.tx_config.idle_level = 0;
+  config.tx_config.idle_level = 1;
 
   // config.tx_config.carrier_duty_percent = 50;
   // set audible career frequency of 611 Hz
@@ -63,7 +63,7 @@ void led_control_hw_init()
   ESP_ERROR_CHECK(rmt_config(&config));
   ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
   // ESP_ERROR_CHECK(rmt_translator_init(config.channel, u8_to_rmt));
-  init_led_data_buffer();
+  init_led_data_buffer(0);
 
   // ESP_ERROR_CHECK(rmt_isr_register(rmt_send_finished_isr, NULL, 0, NULL));
 }
@@ -72,26 +72,33 @@ void led_control_hw_init()
 //   configPRINTF(("LED Control rmt_write_items FINISHED!\r\n") );
 // }
 
-void init_led_data_buffer(void) 
+void init_led_data_buffer(int a) 
 {
   for (uint32_t led = 0; led < NUM_LEDS; led++) {
-    uint32_t bits_to_send = RGB(0x00, 0xFF, 0x00);
+    uint32_t bits_to_send = a % 2 == 0 ? RGB(0x00, 0x00, 0xFF) : RGB(0x00, 0xFF, 0x00);
 
     for (uint32_t bit = 0; bit < BITS_PER_LED_CMD; bit++) {
       uint32_t value = bits_to_send | (uint32_t)1;
 
-      led_data_buffer[led * BITS_PER_LED_CMD + bit] = value ? 
+      led_data_buffer[led * BITS_PER_LED_CMD + bit + 1] = value ?  // + 1 to skip the first one for the reset
                                                       (rmt_item32_t){{{T1H, 1, TL, 0}}} : 
                                                       (rmt_item32_t){{{T0H, 1, TL, 0}}};
     }
   }
+
+  // Add a LOW period at the start and end of the transmission
+  led_data_buffer[0] = (rmt_item32_t){{{1, 1, 45, 0}}};
+  led_data_buffer[LED_BUFFER_ITEMS - 1] = (rmt_item32_t){{{1, 1, 45, 0}}};
 }
 
+int led_cycle = 0;
 
 void led_control_hw_test() {
   configPRINTF(("LED Control starting rmt_write_items...\r\n") );
   ESP_ERROR_CHECK(rmt_write_items(LED_RMT_TX_CHANNEL, led_data_buffer, LED_BUFFER_ITEMS, false));
   ESP_ERROR_CHECK(rmt_wait_tx_done(LED_RMT_TX_CHANNEL, portMAX_DELAY));
+  init_led_data_buffer(led_cycle);
+  led_cycle++;
   configPRINTF(("Transmission complete...\r\n") );
 }
 
