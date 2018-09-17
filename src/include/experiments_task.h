4d63c9706e2789b64de13f9ecf2d6a2c3cd3c3de
Jordan Schaenzle <jschaenzle@gmail.com>
2018-09-17 13:10:46 -0400

    Fixed compile error

--- src/include/experiments_task.h
+++ src/include/experiments_task.h
@@ -2,6 +2,6 @@
 #define EXPERIMENTS_TASK_H
 #include "app_state.h"
 
-void led_cycle_task_start(struct app_state * p_app_state);
+void experiments_task_start(struct app_state * p_app_state);
 
 #endif
