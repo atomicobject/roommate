ab1773611f3b96403468c4e6d6e3f00d41cef9c2
Jordan Schaenzle <jschaenzle@gmail.com>
2018-10-10 21:55:27 -0400

    Added unit test support with Unity

---
+++ test/test_src/calendar_event_processing_tests.c
@@ -0,0 +1,17 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(CalendarEventProcessing);
+
+TEST_SETUP(CalendarEventProcessing)
+{
+}
+
+TEST_TEAR_DOWN(CalendarEventProcessing)
+{
+}
+
+TEST(CalendarEventProcessing, test_some_things)
+{
+  TEST_ASSERT_EQUAL(0, 0);
+}
---
+++ test/test_src/test_runners/calendar_event_processing_runner.c
@@ -0,0 +1,12 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(CalendarEventProcessing)
+{
+  RUN_TEST_CASE(CalendarEventProcessing, test_some_things);
+  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnZeroIfItemIsNotInList_WhichWorksEvenInOurBrokenCode);
+  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnTheIndexForItemsInList_WhichWillFailBecauseOurFunctionUnderTestIsBroken);
+  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValue);
+  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValueAgain);
+  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnCurrentCounter_ButFailsBecauseThisTestIsActuallyFlawed);
+}
