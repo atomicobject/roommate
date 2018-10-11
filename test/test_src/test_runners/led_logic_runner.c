61e8d86d147c526cbb62633c46e13fb726a92dee
Jordan Schaenzle <jschaenzle@gmail.com>
2018-10-10 22:04:31 -0400

    Removed commented out code

--- test/test_src/test_runners/led_logic_runner.c
+++ test/test_src/test_runners/led_logic_runner.c
@@ -4,9 +4,4 @@
 TEST_GROUP_RUNNER(LEDLogic)
 {
   RUN_TEST_CASE(LEDLogic, all_leds_should_be_green_when_no_meetings_scheduled);
-  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnZeroIfItemIsNotInList_WhichWorksEvenInOurBrokenCode);
-  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnTheIndexForItemsInList_WhichWillFailBecauseOurFunctionUnderTestIsBroken);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValue);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValueAgain);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnCurrentCounter_ButFailsBecauseThisTestIsActuallyFlawed);
 }
