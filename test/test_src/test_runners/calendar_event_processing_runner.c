5605d3a7ed8e88381304b4919535a74d4a88cd11
Jordan Schaenzle <jschaenzle@gmail.com>
2018-10-10 22:04:31 -0400

    Removed commented out code

--- test/test_src/test_runners/calendar_event_processing_runner.c
+++ test/test_src/test_runners/calendar_event_processing_runner.c
@@ -4,9 +4,4 @@
 TEST_GROUP_RUNNER(CalendarEventProcessing)
 {
   RUN_TEST_CASE(CalendarEventProcessing, test_some_things);
-  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnZeroIfItemIsNotInList_WhichWorksEvenInOurBrokenCode);
-  // RUN_TEST_CASE(ProductionCode, FindFunction_WhichIsBroken_ShouldReturnTheIndexForItemsInList_WhichWillFailBecauseOurFunctionUnderTestIsBroken);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValue);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValueAgain);
-  // RUN_TEST_CASE(ProductionCode, FunctionWhichReturnsLocalVariable_ShouldReturnCurrentCounter_ButFailsBecauseThisTestIsActuallyFlawed);
 }
