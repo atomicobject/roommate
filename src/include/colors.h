29915091454964e6a4e5a7301401fb1d657f6577
John Ruble <john.ruble@atomicobject.com>
2018-12-28 15:58:30 -0500

    gamma map colors

--- src/include/colors.h
+++ src/include/colors.h
@@ -2,13 +2,15 @@
 #define COLORS_H
 #include "led_utils.h"
 
-#define AO_GREEN RGBB(0x00, 0xED, 0x9D, 0.35)
-#define AO_RED RGBB(0xF0, 0x59, 0x5C, 0.35)
-#define AO_YELLOW RGBB(0xDC, 0xAD, 0x66, 0.25)
+#define AO_GREEN RGBB(0x00, 0xCD, 0xBD, 0.5)
+#define AO_RED RGBB(0xFD, 0x45, 0x57, 0.5)
+#define AO_YELLOW RGBB(0xDC, 0xAD, 0x66, 0.5)
 
 #define RED 0x2A0000
 #define BLUE 0x00002A
 #define GREEN 0x002A00
 #define LED_OFF 0x000000
+
+
 
 #endif
